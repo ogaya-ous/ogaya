cb8dd8c1d6bd9316bb6d3b767d4652639346089d
iruk23 <inde0517@gmail.com>
2022-12-07 05:48:17 +0900

    add memo

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,16 @@ import mysql from 'mysql2';
 
 const app = express();
 
-// MySQLとの接続情報を定義
-const connection = mysql.createConnection({
+// MySQLとの接続情報を定義 (document_db)
+const connection_doc = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
   password: '',
   database: 'document_db'
 });
 
-connection.connect((err) => {
+
+connection_doc.connect((err) => {
   if (err) {
     // エラーが発生した場合は、エラーを表示
     console.error(err.message);
@@ -35,6 +36,7 @@ const storage = multer.diskStorage({
 // 画像をアップロードするためのミドルウェア
 const upload = multer({ storage: storage });
 
+
 // ファイルアップロード用のルーティングを設定する
 app.post('/upload', upload.single('image'), (req, res) => {
   // アップロードされた画像が存在しない場合は、処理を中断する
@@ -54,8 +56,8 @@ app.post('/upload', upload.single('image'), (req, res) => {
   const imageTitle = req.body.title;
 
   // データベースに接続し、SQLを実行
-  connection.query(
-    "INSERT INTO DOC VALUES (13, (?), (?), '20221202', 0)",
+  connection_doc.query(
+    "INSERT INTO document VALUES (13, (?), (?), '20221202', 0)",
     [imageTitle, imagePath],
     function(error, results, fields) {
 
@@ -73,8 +75,57 @@ app.post('/upload', upload.single('image'), (req, res) => {
 });
 
 
+app.post('/comment', upload.single('comment'), (req, res) => {
+  //コメントが空の場合は，処理を中断する
+  if (!req.body.comment) {
+    res.status(400).send('テキストが入力されていません');
+    return;
+  }
+
+  // bodyのコメントを取得
+  const commant = req.body.comment;
+
+
+})
+
 // Webサーバーを起動する
 app.listen(8000, function() {
   console.log('Example app listening on port 8000!');
-  // uploadImage(app)
 });
+
+
+
+/*
+
+テーブル作成例メモ
+
+CREATE TABLE document (
+  document_id INT NOT NULL AUTO_INCREMENT,
+  document_name VARCHAR(255) NOT NULL,
+  owner VARCHAR(255) NOT NULL,
+  collection_site VARCHAR(255) NOT NULL,
+  discovery_year INT NOT NULL,
+  discovery_location VARCHAR(255) NOT NULL,
+  content VARCHAR(255) NOT NULL,
+  PRIMARY KEY (document_id)
+  );
+
+  CREATE TABLE decoder (
+  decoder_id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  affiliation VARCHAR(255) NOT NULL,
+  specialty VARCHAR(255) NOT NULL,
+  PRIMARY KEY (decoder_id)
+  );
+
+  CREATE TABLE history (
+  history_id INT NOT NULL AUTO_INCREMENT,
+  document_id INT NOT NULL,
+  decoder_id INT NOT NULL,
+  decoding_datetime DATETIME NOT NULL,
+  decoding_content VARCHAR(255) NOT NULL,
+  PRIMARY KEY (history_id),
+  FOREIGN KEY (document_id) REFERENCES document (document_id),
+  FOREIGN KEY (decoder_id) REFERENCES decoder (decoder_id)
+  );
+*/
