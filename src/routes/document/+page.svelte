738d3a1dcf5320c220f187f86bca73d2139a2823
SoraAsada <asasora7326@gmail.com>
2022-12-13 18:59:53 +0900

    asada document

--- src/routes/document/+page.svelte
+++ src/routes/document/+page.svelte
@@ -1,13 +1,21 @@
 <script>
-	
+	import img_path from '$lib/images/img_doc1.jpg';
 </script>
 
 <main>
+    <section id = "work" class = "cf">
+        <h2>文書一覧</h2>
+        <article>
+            <img src={ img_path } width="313" height="269" alt="doc_1">
+            <h3>文書一巻</h3>
+            <p>この文書は、大正時代に起こったたたら爆発の事件について記載されている。</p>
+        </article>
 
+    </section>
 
 </main>
 
 <style>
 
-    
+
 </style>
