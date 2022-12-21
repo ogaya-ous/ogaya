71ffc9590640ab2e9e0cce327f84723279a64d49
t19j068 <t19j068hk@ous.jp>
2022-12-21 17:04:52 +0900

    [fix] footer responsive design.

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -50,12 +50,18 @@
         </div>
         <div class="right">
             <ul class="list_style">
-                <li>Home</li>
-                <li>News</li>
-                <li>Document</li>
-                <li>History</li>
-                <li>Contact</li>
-                <li>Login</li>
+				<div class="sp_column">
+					<div class="left_item">
+						<li>Home</li>
+						<li>News</li>
+						<li>Document</li>
+					</div>
+					<div class="right_item">
+						<li>History</li>
+						<li>Contact</li>
+						<li>Login</li>
+					</div>
+				</div>
             </ul>
         </div>
     </div>
@@ -65,105 +71,245 @@
 </footer>
 
 <style>
-    * {
+	* {
 		margin: 0;
 		padding: 0;
 	}
 
-    footer {
-        color: white;
-        background-color: #303030;
-        font-family: 'Noto Serif JP', serif;
-        font-weight: 300;
-        clear:both;
-    }
+	footer {
+		display: block;
+		color: #ffffff;
+		background-color: #303030;
+		font-family: 'Noto Serif JP', serif;
+		font-weight: 300;
+		clear:both;
+	}
 
-    .list_style {
-        list-style: none;
-    }
+	.list_style {
+		list-style: none;
+	}
 
-    footer .wrap {
-        padding: 40px 80px;
-        display: flex;
-        justify-content: space-around;
-    }
+	footer .wrap {
+		padding: 60px 0 0;
+	}
 
-    footer .wrap .left {
-        letter-spacing: 2px;
-    }
+	footer .wrap .left {
+		letter-spacing: 2px;
+		margin-left: 30px;
+		margin-bottom: 20px;
+	}
 
-    footer .wrap .left li {
-        line-height: 1.5;
-    }
+	footer .wrap .left li {
+		line-height: 1.5;
+	}
 
-    footer .wrap .left li.title {
-        font-size: 24px;
-    }
-    
-    footer .wrap .left .sns_icon {
-        display: flex;
-        margin-top: 10px;
-    }
+	footer .wrap .left li.title {
+		font-size: 20px;
+	}
 
-    footer .wrap .left .sns_icon li {
-        margin-right: 25px;
-    }
+	footer .wrap .left li.catch_copy {
+		font-size: 14px;
+	}
+	
+	footer .wrap .left .sns_icon {
+		display: flex;
+		margin-top: 10px;
+	}
 
-    footer .wrap .center {
-        margin-left: 0;
-        letter-spacing: 2px;
-    }
+	footer .wrap .left .sns_icon li {
+		margin-right: 20px;
+	}
 
-    footer .wrap .center li {
-        line-height: 2;
-    }
+	footer .wrap .left .sns_icon img {
+		width: 70%;
+		height: auto;
+	}
 
-    footer .wrap .center li.address_erea {
-        display: flex;
-    }
+	footer .wrap .center {
+		margin-left: 0;
+		letter-spacing: 2px;
+		font-size: 14px;
+	}
 
-    footer .wrap .center li.address_erea .address,
-    footer .wrap .center li.contact_erea .contact {
-        width: 150px;
-    }
+	footer .wrap .center .address_erea,
+	footer .wrap .center .contact_erea {
+		margin-left: 30px;
+		margin-bottom: 20px;
+	}
 
-    footer .wrap .center li.address_erea .ogaya .ogaya_left .name,
-    footer .wrap .center li.contact_erea .council .ogaya_left .name {
-        width: 100px;
-    }
+	footer .wrap .center li {
+		line-height: 2;
+	}
 
-    footer .wrap .center li.address_erea .ogaya .ogaya_right a {
-        text-decoration: none;
-        color: white;
-    }
+	footer .wrap .center li.address_erea .ogaya .ogaya_right a {
+		text-decoration: none;
+		color: white;
+	}
 
-    footer .wrap .center li.address_erea .ogaya {
-        display: flex;
-    }
+	footer .wrap .right {
+		letter-spacing: 2px;
+		font-size: 14px;
+		margin-left: 30px;
+		margin-bottom: 20px;
+	}
 
-    footer .wrap .center li.contact_erea {
-        display: flex;
-        margin-top: 20px;
-    }
+	footer .wrap .right .sp_column {
+		display: flex;
+	}
 
-    footer .wrap .center li.contact_erea .council {
-        display: flex;
-    }
+	footer .wrap .right .sp_column .right_item {
+		margin-left: 30px;
+	}
 
-    footer .wrap .right {
-        margin-left: 100px;
-        letter-spacing: 2px;
-    }
+	footer .wrap .right li {
+		line-height: 2;
+	}
 
-    footer .wrap .right li {
-        line-height: 2;
-    }
+	footer .copyright {
+		font-size: 10px;
+		text-align: right;
+		padding: 5px 5px 5px 0;
+		border-top: 1px white solid;
+		letter-spacing: 1px;
+	}
 
-    footer .copyright {
-        font-size: 13px;
-        text-align: right;
-        padding: 20px 60px;
-        border-top: 1px white solid;
-        letter-spacing: 1px;
-    }
+	@media screen and (min-width:480px) {
+		footer .wrap .left {
+			margin-left: 60px;
+		}
+
+		footer .wrap .center .address_erea,
+		footer .wrap .center .contact_erea {
+			margin-left: 60px;
+		}
+
+		footer .wrap .right {
+			margin-left: 60px;
+		}
+
+		footer .wrap .right .sp_column .right_item {
+			margin-left: 60px;
+		}
+	}
+
+	@media screen and (min-width:768px) and ( max-width:1024px) {
+		footer .wrap .center li.address_erea {
+			display: flex;
+		}
+
+		footer .wrap .center li.address_erea .address,
+		footer .wrap .center li.contact_erea .contact {
+			width: 150px;
+		}
+
+		footer .wrap .center li.address_erea .ogaya .ogaya_left .name,
+		footer .wrap .center li.contact_erea .council .ogaya_left .name {
+			width: 100px;
+		}
+
+		footer .wrap .center li.address_erea .ogaya {
+			display: flex;
+		}
+
+		footer .wrap .center li.contact_erea {
+			display: flex;
+			margin-top: 20px;
+		}
+
+		footer .wrap .center li.contact_erea .council {
+			display: flex;
+		}
+	}
+
+	@media screen and (min-width:1024px) {
+		footer {
+			display: block;
+		}
+		
+		footer .wrap {
+			padding: 40px 80px;
+			display: flex;
+			justify-content: space-around;
+		}
+
+		footer .wrap .left {
+			margin-left: 0;
+			margin-bottom: 0;
+		}
+
+		footer .wrap .left li.title {
+			font-size: 24px;
+		}
+
+		footer .wrap .left li.catch_copy {
+			font-size: 16px;
+		}
+
+		footer .wrap .left .sns_icon li {
+			margin-right: 25px;
+		}
+
+		footer .wrap .left .sns_icon img {
+			width: 100%;
+			height: auto;
+		}
+
+		footer .wrap .center {
+			font-size: 16px;
+		}
+
+		footer .wrap .center .address_erea,
+		footer .wrap .center .contact_erea {
+			margin-left: 0;
+			margin-bottom: 0;
+		}
+
+		footer .wrap .center li.address_erea {
+			display: flex;
+		}
+
+		footer .wrap .center li.address_erea .address,
+		footer .wrap .center li.contact_erea .contact {
+			width: 150px;
+		}
+
+		footer .wrap .center li.address_erea .ogaya .ogaya_left .name,
+		footer .wrap .center li.contact_erea .council .ogaya_left .name {
+			width: 100px;
+		}
+
+		footer .wrap .center li.address_erea .ogaya {
+			display: flex;
+		}
+
+		footer .wrap .center li.contact_erea {
+			display: flex;
+			margin-top: 20px;
+		}
+
+		footer .wrap .center li.contact_erea .council {
+			display: flex;
+		}
+
+		footer .wrap .right {
+			letter-spacing: 2px;
+			font-size: 16px;
+			margin-left: 100px;
+			margin-bottom: 0;
+		}
+
+		footer .wrap .right .sp_column {
+			display: block;
+		}
+
+		footer .wrap .right .sp_column .right_item {
+			margin-left: 0;
+		}
+
+		footer .copyright {
+			font-size: 13px;
+			text-align: right;
+			padding: 20px 60px;
+		}
+	}
 </style>
