af0a123dcb2815aca345dfb440f085a3e7d1eb24
SoraAsada <asasora7326@gmail.com>
2022-12-23 01:20:37 +0900

    news

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,11 +1,21 @@
 <script>
-
+    import img_path from '$lib/images/news.jpg';
 </script>
 
 <main>
-    <p>news</p>
+    <div class="headline">
+        <h2>news</h2>
+    </div>
 </main>
 
 <style>
+    .headline {
+        background-image: url(../../lib/images/news.jpg);
+        height: 60px;
+    }
+
+    .headline h2 {
+        color: #fff;
+    }
 
 </style>
