2762ac55c3fe20c381fdd55557a25280e4b1b01b
iruk23 <inde0517@gmail.com>
2023-12-08 07:25:05 +0900

    fsfasfs

--- api/index.js
+++ api/index.js
@@ -13,4 +13,8 @@ app.get('/api/item/:slug', (req, res) => {
   res.end(`Item: ${slug}`);
 });
 
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
+
 export default app;
