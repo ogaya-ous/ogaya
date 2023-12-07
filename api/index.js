02bcff73066a407f3437aae226b9b1015300b4d7
iruk23 <inde0517@gmail.com>
2023-12-08 06:08:05 +0900

    sine

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
-const app = require('express')();
-const { v4 } = require('uuid');
+// const app = require('express')();
+// const { v4 } = require('uuid');
+import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
+const app = express();
 
 app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
+  const path = `/api/item/${uuidv4()}`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
