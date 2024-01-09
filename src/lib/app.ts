aae30b8f2c4e27d476701f3f03ebe4792025ed58
iruk23 <inde0517@gmail.com>
2024-01-09 12:08:27 +0900

    a

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,34 +1,32 @@
 import { browser } from '$app/environment';
 import type { FirebaseApp, FirebaseOptions } from 'firebase/app';
+import { initializeApp } from 'firebase/app';
 import { readable } from 'svelte/store';
 
-/*
-const firebaseConfig: FirebaseOptions = {
-    apiKey: process.env['FIREBASE_API_KEY'],
-    authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
-    projectId: process.env['FIREBASE_PROJECT_ID'],
-    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
-    messagingSenderId: process.env['FIREBASE_MESSAGE_SENDER_ID'],
-    appId: process.env['FIREBASE_APP_ID'],
-}*/
-const firebaseConfig: FirebaseOptions = {
-    apiKey: "AIzaSyDENWIbvQodB8MbZXispbqxw4-FWl2vcVk",
-    authDomain: "ogaya-8e672.firebaseapp.com",
-    projectId: "ogaya-8e672",
-    storageBucket: "ogaya-8e672.appspot.com",
-    messagingSenderId: "552254909048",
-    appId: "1:552254909048:web:1c4ced2226d2a840d31b8d"
-};
+export const firebaseConfig: FirebaseOptions = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+}
+
+// console.log(firebaseConfig)
+
 
 function createApp() {
     let app: FirebaseApp
+    console.log('create app')
 
     const { subscribe } = readable<FirebaseApp>(undefined, (set) => {
+        console.log('subscribe')
         async function init() {
             if (!app) {
-                const { initializeApp } = await import('firebase/app')
                 app = initializeApp(firebaseConfig)
+                console.log('app initialized')
             }
+            console.log('not initialized')
             set(app)
         }
 
