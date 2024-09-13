423342076a4ee3d42a4e6115e09d15837de524f3
iruk23 <inde0517@gmail.com>
2024-09-13 17:30:56 +0900

    add permission

--- prisma/migrations/20240822065620_decoded/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "decoded" ALTER COLUMN "decoded_name" SET DATA TYPE TEXT;
--- prisma/migrations/20240913060431_add_role/migration.sql
+++ prisma/migrations/20240913060431_add_role/migration.sql
@@ -1,28 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `decoding_datetime` on the `history` table. All the data in the column will be lost.
-  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
-  - Added the required column `added_day` to the `history` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `added_month` to the `history` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `added_year` to the `history` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `decoding_gpt` to the `history` table without a default value. This is not possible if the table is not empty.
-
-*/
--- DropForeignKey
-ALTER TABLE "history" DROP CONSTRAINT "history_user_id_fkey";
-
--- AlterTable
-ALTER TABLE "history" DROP COLUMN "decoding_datetime",
-ADD COLUMN     "added_day" INTEGER NOT NULL,
-ADD COLUMN     "added_month" INTEGER NOT NULL,
-ADD COLUMN     "added_year" INTEGER NOT NULL,
-ADD COLUMN     "decoding_gpt" TEXT NOT NULL,
-ALTER COLUMN "user_id" SET DATA TYPE TEXT;
-
--- DropTable
-DROP TABLE "user";
-
 -- CreateTable
 CREATE TABLE "Account" (
     "id" TEXT NOT NULL,
@@ -58,6 +33,7 @@ CREATE TABLE "User" (
     "email" TEXT,
     "emailVerified" TIMESTAMP(3),
     "image" TEXT,
+    "role" TEXT DEFAULT 'USER',
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -70,6 +46,20 @@ CREATE TABLE "VerificationToken" (
 );
 
 -- CreateTable
+CREATE TABLE "document" (
+    "document_id" SERIAL NOT NULL,
+    "document_name" TEXT NOT NULL,
+    "document_path" TEXT NOT NULL,
+    "document_explain" TEXT NOT NULL,
+    "added_year" INTEGER NOT NULL,
+    "added_month" INTEGER NOT NULL,
+    "added_day" INTEGER NOT NULL,
+    "delete_flag" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "document_pkey" PRIMARY KEY ("document_id")
+);
+
+-- CreateTable
 CREATE TABLE "image" (
     "image_id" SERIAL NOT NULL,
     "document_id" INTEGER NOT NULL,
@@ -79,6 +69,49 @@ CREATE TABLE "image" (
     "added_day" INTEGER NOT NULL,
 
     CONSTRAINT "image_pkey" PRIMARY KEY ("image_id")
+);
+
+-- CreateTable
+CREATE TABLE "history" (
+    "history_id" SERIAL NOT NULL,
+    "document_id" INTEGER NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "added_year" INTEGER NOT NULL,
+    "added_month" INTEGER NOT NULL,
+    "added_day" INTEGER NOT NULL,
+    "decoding_content" TEXT NOT NULL,
+    "decoding_gpt" TEXT NOT NULL,
+
+    CONSTRAINT "history_pkey" PRIMARY KEY ("history_id")
+);
+
+-- CreateTable
+CREATE TABLE "news" (
+    "news_id" SERIAL NOT NULL,
+    "news_name" TEXT NOT NULL,
+    "news_path" TEXT NOT NULL,
+    "news_explain" TEXT NOT NULL,
+    "added_year" INTEGER NOT NULL,
+    "added_month" INTEGER NOT NULL,
+    "added_day" INTEGER NOT NULL,
+    "delete_flag" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "news_pkey" PRIMARY KEY ("news_id")
+);
+
+-- CreateTable
+CREATE TABLE "decoded" (
+    "decoded_id" SERIAL NOT NULL,
+    "decoded_name" TEXT NOT NULL,
+    "decoded_path" TEXT NOT NULL,
+    "decoded_explain" TEXT NOT NULL,
+    "decoded_content" TEXT NOT NULL,
+    "added_year" INTEGER NOT NULL,
+    "added_month" INTEGER NOT NULL,
+    "added_day" INTEGER NOT NULL,
+    "delete_flag" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "decoded_pkey" PRIMARY KEY ("decoded_id")
 );
 
 -- CreateIndex
@@ -103,7 +136,10 @@ ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId"
 ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "history" ADD CONSTRAINT "history_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "image" ADD CONSTRAINT "image_document_id_fkey" FOREIGN KEY ("document_id") REFERENCES "document"("document_id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "image" ADD CONSTRAINT "image_document_id_fkey" FOREIGN KEY ("document_id") REFERENCES "document"("document_id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "history" ADD CONSTRAINT "history_document_id_fkey" FOREIGN KEY ("document_id") REFERENCES "document"("document_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "history" ADD CONSTRAINT "history_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
