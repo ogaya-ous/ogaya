a18c6e49c8493f79b48f225ef76d30544813380f
iruk23 <inde0517@gmail.com>
2024-09-20 00:09:45 +0900

    不要なパッケージ削除

--- prisma/migrations/20240913074325_add_role/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "decoded" ADD COLUMN     "view_flag" BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE "document" ADD COLUMN     "view_flag" BOOLEAN NOT NULL DEFAULT false;
