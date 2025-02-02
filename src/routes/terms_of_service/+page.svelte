e92138240051d86314c07f9f1da1542a2614e780
iruk23 <inde0517@gmail.com>
2025-02-03 01:00:43 +0900

    test

--- src/routes/terms_of_service/+page.svelte
+++ src/routes/terms_of_service/+page.svelte
@@ -1,160 +1,195 @@
 <script>
-
 </script>
 
 <main>
-    <div class="Inner">
-        <h2 class="Inner-Headline">利用規約</h2>
-        <p class="Inner-Text first">この文書は、ウェブサイト「よってたかって大茅地区」（以下、「当サイト」といいます）の利用に関する利用規約（以下、「本規約」といいます）です。当サイトを利用する際には、本規約をよくお読みいただき、同意の上でご利用いただきますようお願いいたします。</p>
-        <div class="Inner-Box">
-            <h3 class="Inner-Box-Headline">利用規約の受諾</h3>
-            <ol class="Inner-Box-List">
-                <li class="Inner-Box-List-Item">当サイトを利用する際には、本規約および当サイトのプライバシーポリシーに同意していただく必要があります。</li>
-                <li class="Inner-Box-List-Item">未成年者は、親権者または法定代理人の同意を得た上で本サイトをご利用ください。</li>
-            </ol>
-        </div>
-        <div class="Inner-Box">
-            <h3 class="Inner-Box-Headline">サービスの提供</h3>
-            <ol class="Inner-Box-List">
-                <li class="Inner-Box-List-Item">当サイトは、大茅地区の区有文書を掲載し、ユーザーに対してその翻訳を依頼するサービスを提供します。</li>
-                <li class="Inner-Box-List-Item">当サイトは、事前の通知なしにサービスの内容を変更する権利を保有しています。</li>
-            </ol>
-        </div>
-        <div class="Inner-Box">
-            <h3 class="Inner-Box-Headline">ユーザーの責任</h3>
-            <ol class="Inner-Box-List">
-                <li class="Inner-Box-List-Item">ユーザーは、本サイトを利用する際に提供される情報が正確かつ完全であることを確認する責任があります。</li>
-                <li class="Inner-Box-List-Item">ユーザーは、本サイトを不正な目的で利用したり、他のユーザーに対して迷惑行為を行ったりしないように努めなければなりません。</li>
-            </ol>
-        </div>
-        <div class="Inner-Box">
-            <h3 class="Inner-Box-Headline">知的財産権</h3>
-            <ol class="Inner-Box-List">
-                <li class="Inner-Box-List-Item">本サイトに掲載されているコンテンツ（テキスト、画像、ロゴなど）は、当サイトまたは関連する権利者に帰属します。無断で複製、転載、改変、配布することは禁止されています。</li>
-            </ol>
-        </div>
-        <div class="Inner-Box">
-            <h3 class="Inner-Box-Headline">個人情報保護方針</h3>
-            <ol class="Inner-Box-List">
-                <li class="Inner-Box-List-Item">当サイトは、ユーザーの個人情報を適切に取り扱うために努めます。ユーザーから収集した個人情報は、当サイトの「個人情報保護方針」に基づいて管理されます。詳細については、当サイトの個人情報保護方針を参照してください。</li>
-                <li class="Inner-Box-List-Item">ユーザーの個人情報は、法的な要求やサービス提供のために協力する業者との間で共有されることがありますが、これは当サイトの個人情報保護方針に基づいて行われます。</li>
-            </ol>
-        </div>
-        <div class="Inner-Box">
-            <h3 class="Inner-Box-Headline">秘密保持</h3>
-            <ol class="Inner-Box-List">
-                <li class="Inner-Box-List-Item">ユーザーは、本サイトを利用する際に得た情報について機密を守り、第三者に漏洩させないよう努めなければなりません。</li>
-                <li class="Inner-Box-List-Item">当サイトが提供する翻訳サービスに関連する情報や文書にアクセスした場合、その情報は秘密情報と見なされます。</li>
-            </ol>
-        </div>
-        <div class="Inner-Box">
-            <h3 class="Inner-Box-Headline">反社会的勢力の排除</h3>
-            <ol class="Inner-Box-List">
-                <li class="Inner-Box-List-Item">当サイトは、反社会的勢力や犯罪組織との関わりを排除し、安全で健全なオンライン環境を提供するための措置を講じます。</li>
-                <li class="Inner-Box-List-Item">ユーザーは、本サイトを利用する際に反社会的勢力や不正行為に加担しないことを確約します。</li>
-            </ol>
-        </div>
-        <div class="Inner-Box">
-            <h3 class="Inner-Box-Headline">知的財産権</h3>
-            <ol class="Inner-Box-List">
-                <li class="Inner-Box-List-Item">ユーザーが当サイトに提供する翻訳文に関する知的財産権は、原則としてユーザーに帰属します。ただし、当サイトは、提供された翻訳文をサイトの改善や他のユーザーへの提供のために利用する権利を保有します。</li>
-            </ol>
-        </div>
-        <div class="Inner-Box">
-            <h3 class="Inner-Box-Headline">免責事項</h3>
-            <ol class="Inner-Box-List">
-                <li class="Inner-Box-List-Item">当サイトは、サービスの提供に最善を尽くしますが、その正確性や信頼性について一切の保証をしません。</li>
-                <li class="Inner-Box-List-Item">ユーザーは、自己の責任において本サイトを利用し、利用に伴う一切のリスクに対処するものとします。</li>
-            </ol>
-        </div>
-        <div class="Inner-Box">
-            <h3 class="Inner-Box-Headline">規約の変更</h3>
-            <ol class="Inner-Box-List">
-                <li class="Inner-Box-List-Item">当サイトは、必要に応じて本規約を変更する権利を保有しています。変更が行われた場合は、本サイト上で通知いたします。</li>
-            </ol>
-        </div>
-      </div>
+  <div class="Inner">
+    <h2 class="Inner-Headline">利用規約</h2>
+    <p class="Inner-Text first">
+      この文書は、ウェブサイト「よってたかって大茅地区」（以下、「当サイト」といいます）の利用に関する利用規約（以下、「本規約」といいます）です。当サイトを利用する際には、本規約をよくお読みいただき、同意の上でご利用いただきますようお願いいたします。
+    </p>
+    <div class="Inner-Box">
+      <h3 class="Inner-Box-Headline">利用規約の受諾</h3>
+      <ol class="Inner-Box-List">
+        <li class="Inner-Box-List-Item">
+          当サイトを利用する際には、本規約および当サイトのプライバシーポリシーに同意していただく必要があります。
+        </li>
+        <li class="Inner-Box-List-Item">
+          未成年者は、親権者または法定代理人の同意を得た上で本サイトをご利用ください。
+        </li>
+      </ol>
+    </div>
+    <div class="Inner-Box">
+      <h3 class="Inner-Box-Headline">サービスの提供</h3>
+      <ol class="Inner-Box-List">
+        <li class="Inner-Box-List-Item">
+          当サイトは、大茅地区の区有文書を掲載し、ユーザーに対してその翻訳を依頼するサービスを提供します。
+        </li>
+        <li class="Inner-Box-List-Item">
+          当サイトは、事前の通知なしにサービスの内容を変更する権利を保有しています。
+        </li>
+      </ol>
+    </div>
+    <div class="Inner-Box">
+      <h3 class="Inner-Box-Headline">ユーザーの責任</h3>
+      <ol class="Inner-Box-List">
+        <li class="Inner-Box-List-Item">
+          ユーザーは、本サイトを利用する際に提供される情報が正確かつ完全であることを確認する責任があります。
+        </li>
+        <li class="Inner-Box-List-Item">
+          ユーザーは、本サイトを不正な目的で利用したり、他のユーザーに対して迷惑行為を行ったりしないように努めなければなりません。
+        </li>
+      </ol>
+    </div>
+    <div class="Inner-Box">
+      <h3 class="Inner-Box-Headline">知的財産権</h3>
+      <ol class="Inner-Box-List">
+        <li class="Inner-Box-List-Item">
+          本サイトに掲載されているコンテンツ（テキスト、画像、ロゴなど）は、当サイトまたは関連する権利者に帰属します。無断で複製、転載、改変、配布することは禁止されています。
+        </li>
+      </ol>
+    </div>
+    <div class="Inner-Box">
+      <h3 class="Inner-Box-Headline">個人情報保護方針</h3>
+      <ol class="Inner-Box-List">
+        <li class="Inner-Box-List-Item">
+          当サイトは、ユーザーの個人情報を適切に取り扱うために努めます。ユーザーから収集した個人情報は、当サイトの「個人情報保護方針」に基づいて管理されます。詳細については、当サイトの個人情報保護方針を参照してください。
+        </li>
+        <li class="Inner-Box-List-Item">
+          ユーザーの個人情報は、法的な要求やサービス提供のために協力する業者との間で共有されることがありますが、これは当サイトの個人情報保護方針に基づいて行われます。
+        </li>
+      </ol>
+    </div>
+    <div class="Inner-Box">
+      <h3 class="Inner-Box-Headline">秘密保持</h3>
+      <ol class="Inner-Box-List">
+        <li class="Inner-Box-List-Item">
+          ユーザーは、本サイトを利用する際に得た情報について機密を守り、第三者に漏洩させないよう努めなければなりません。
+        </li>
+        <li class="Inner-Box-List-Item">
+          当サイトが提供する翻訳サービスに関連する情報や文書にアクセスした場合、その情報は秘密情報と見なされます。
+        </li>
+      </ol>
+    </div>
+    <div class="Inner-Box">
+      <h3 class="Inner-Box-Headline">反社会的勢力の排除</h3>
+      <ol class="Inner-Box-List">
+        <li class="Inner-Box-List-Item">
+          当サイトは、反社会的勢力や犯罪組織との関わりを排除し、安全で健全なオンライン環境を提供するための措置を講じます。
+        </li>
+        <li class="Inner-Box-List-Item">
+          ユーザーは、本サイトを利用する際に反社会的勢力や不正行為に加担しないことを確約します。
+        </li>
+      </ol>
+    </div>
+    <div class="Inner-Box">
+      <h3 class="Inner-Box-Headline">知的財産権</h3>
+      <ol class="Inner-Box-List">
+        <li class="Inner-Box-List-Item">
+          ユーザーが当サイトに提供する翻訳文に関する知的財産権は、原則としてユーザーに帰属します。ただし、当サイトは、提供された翻訳文をサイトの改善や他のユーザーへの提供のために利用する権利を保有します。
+        </li>
+      </ol>
+    </div>
+    <div class="Inner-Box">
+      <h3 class="Inner-Box-Headline">免責事項</h3>
+      <ol class="Inner-Box-List">
+        <li class="Inner-Box-List-Item">
+          当サイトは、サービスの提供に最善を尽くしますが、その正確性や信頼性について一切の保証をしません。
+        </li>
+        <li class="Inner-Box-List-Item">
+          ユーザーは、自己の責任において本サイトを利用し、利用に伴う一切のリスクに対処するものとします。
+        </li>
+      </ol>
+    </div>
+    <div class="Inner-Box">
+      <h3 class="Inner-Box-Headline">規約の変更</h3>
+      <ol class="Inner-Box-List">
+        <li class="Inner-Box-List-Item">
+          当サイトは、必要に応じて本規約を変更する権利を保有しています。変更が行われた場合は、本サイト上で通知いたします.
+        </li>
+      </ol>
+    </div>
+  </div>
 </main>
 
 <style>
-.Inner {
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 1200px;
-}
+  .Inner {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 1200px;
+  }
 
-p{
-  margin: 0;
-  padding: 0;
-}
-li{
-  list-style: none;
-}
+  p {
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    list-style: none;
+  }
 
-.Inner-Headline {
-  font-weight: bold;
-  font-size: 32px;
-}
-@media screen and (max-width: 480px) {
   .Inner-Headline {
-    font-size: 24px;
+    font-weight: bold;
+    font-size: 32px;
   }
-}
-.Inner-Text {
-  letter-spacing: 0.05em;
-  line-height: 1.8;
-  font-size: 15px;
-}
-.Inner-Text.first {
-  margin-top: 36px;
-}
-.Inner-Box {
-  margin-top: 16px;
-}
-.Inner-Box-Headline {
-  font-weight: bold;
-  font-size: 28px;
-}
-@media screen and (max-width: 480px) {
+  @media screen and (max-width: 480px) {
+    .Inner-Headline {
+      font-size: 24px;
+    }
+  }
+  .Inner-Text {
+    letter-spacing: 0.05em;
+    line-height: 1.8;
+    font-size: 15px;
+  }
+  .Inner-Text.first {
+    margin-top: 36px;
+  }
+  .Inner-Box {
+    margin-top: 16px;
+  }
   .Inner-Box-Headline {
-    font-size: 20px;
+    font-weight: bold;
+    font-size: 28px;
   }
-}
-.Inner-Box-List {
-  margin: 20px 0 0 40px;
-}
-.Inner-Box-List-Item {
-  margin-top: 10px;
-  letter-spacing: 0.05em;
-  line-height: 1.8;
-  list-style-type: decimal;
-}
-.Inner-Box-List-Item-Ol {
-  margin: 20px 0 0 40px;
-}
-.Inner-Box-List-Item-Ol-Item {
-  margin-top: 10px;
-  letter-spacing: 0.05em;
-  line-height: 1.8;
-  list-style-type: decimal;
-}
-.Inner-Box-List-Item-Link {
-  text-decoration: underline;
-  display: inline-block;
-  color: red;
-}
-.Inner-Box-Contact {
-  margin-top: 36px;
-}
-.Inner-Box-Contact-Text {
-  letter-spacing: 0.05em;
-  line-height: 1.8;
-  font-size: 15px;
-}
-.Inner-Box-Contact-Text:nth-child(2) {
-  margin-top: 16px;
-}
-.Inner-Box-Item {
-  letter-spacing: 0.05em;
-}
+  @media screen and (max-width: 480px) {
+    .Inner-Box-Headline {
+      font-size: 20px;
+    }
+  }
+  .Inner-Box-List {
+    margin: 20px 0 0 40px;
+  }
+  .Inner-Box-List-Item {
+    margin-top: 10px;
+    letter-spacing: 0.05em;
+    line-height: 1.8;
+    list-style-type: decimal;
+  }
+  .Inner-Box-List-Item-Ol {
+    margin: 20px 0 0 40px;
+  }
+  .Inner-Box-List-Item-Ol-Item {
+    margin-top: 10px;
+    letter-spacing: 0.05em;
+    line-height: 1.8;
+    list-style-type: decimal;
+  }
+  .Inner-Box-List-Item-Link {
+    text-decoration: underline;
+    display: inline-block;
+    color: red;
+  }
+  .Inner-Box-Contact {
+    margin-top: 36px;
+  }
+  .Inner-Box-Contact-Text {
+    letter-spacing: 0.05em;
+    line-height: 1.8;
+    font-size: 15px;
+  }
+  .Inner-Box-Contact-Text:nth-child(2) {
+    margin-top: 16px;
+  }
+  .Inner-Box-Item {
+    letter-spacing: 0.05em;
+  }
 </style>
